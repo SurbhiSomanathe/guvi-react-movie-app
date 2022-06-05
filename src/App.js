39da6d22f49e84671e509960eb4c51e263d9d46f
surbhisomnathe <surbhisomnathe7777@gmail.com>
2022-06-05 12:53:08 +0530

    for movie aap

--- src/App.js
+++ src/App.js
@@ -1,62 +1,152 @@
+
+import { useState } from "react";
 import "./App.css";
 function App() {
   // const name = "Divya";
   // const time = 30;
 
+
 const names = ["KimNamJoon","KimSeokJin","MinYoongi","JungHoseok","ParkJimin","KimTaehyung","JeonJungkook","BTS"];
 
 const students = [
-  { 
-    name:"KimNamJoon",
-    pic:"https://lh3.googleusercontent.com/cVA7oKmFjdjnaVY7D39HOiuLVGbn0QPEfBNKkvZ2hsxdaVGuX5DRBcGlQUN6e9MSM_9690Xabpo2PcC6K5eyQJ5z"
-  },
- {
-   name:"KimSeokJin",
-   pic:"https://lh3.googleusercontent.com/rsinNbEDPHC6RzCyBHdQJFgqWkkz0g_fb_iEqq2fcMad-orgGSZx_FjU65Q7m7IA6Wve_Hrts5IlGGlIuUzq75E"
-  },
-{
-  name:"MinYoongi",
-  pic:"https://lh3.googleusercontent.com/cyWtWiqpuQwszSl8gX2IwMT_h11PQBCg_B5GutW1q2Ex0krpY_JCjIMbJ1AhQZxrs7CnvR89VIPdJ4B2mUA4yXM01Q"
-},
-{
-  name:"JungHoseok" ,
-  pic:"https://lh3.googleusercontent.com/gCZfFx5gVLvXnNLw_SWHNZDf55RYNTVdRVhKrYK10rmS1C1vfg0yKSFlSXJ3IZrPP6hseWgEv6jf7vxv7itTpJIc5A"
-},
-{
-  name:"ParkJimin" ,
-  pic:"https://lh3.googleusercontent.com/u3g34k-ainjxEFOyJ58-4Dvch-uqQ6q2mhN_JT3E2wHjbv-LpmCR_-4Uowdt_A5cdQyD4eDWIZvm-Hpy7WgrzeK7YQ"
-},
-
-{
-  name:"KimTaehyung" ,
-  pic:"https://lh3.googleusercontent.com/P8iszSIOvGKrqWYo_DYebQ5FnxH1IbgumJX0hkEGYdEoH98cJFsP8j4y8Z49HpL_KqzYkEoy394QZ2q7MpCJsHPIXA"
-},
-
-{
-  name:"JeonJungkook" ,
-  pic:"https://lh3.googleusercontent.com/JKKCfUNHJytFHO9WMyjnVdbC2UrAGmszPNQxbMPieQz4OJ0EW9GmXlZDYc-bq-qPqrDHWscStV4YguiF9Ux1nWY"
-},
-
-{
-  name:"BTS" ,
-  pic:"https://lh3.googleusercontent.com/z2Nx3OKs17IJ72sR8tjk124xgfgGJPw80NeOfO9nAvQ34WFWFwqi-ndFQzQKpdZ4rdnGqBH7c0rVTrcaJF4vTHs"
-},
-
 ];
+const movieList = [
+  {
+  name: "RRR",
+  poster:
+  "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+  rating: 8.8,
+  summary:
+  "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+  
+  },
+  {
+  name: "Iron man 2",
+  poster:
+  "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+  rating: 7,
+  summary:
+  "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+  
+  },
+  {
+  name: "No Country for Old Men",
+  poster:
+  "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+  rating: 8.1,
+  summary:
+  "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+  
+  },
+  {
+  name: "Jai Bhim",
+  poster:
+  "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+  summary:
+  "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+  rating: 8.8,
+  
+  },
+  {
+  name: "The Avengers",
+  rating: 8,
+  summary:
+  "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+  poster:
+  "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+  
+  },
+  {
+  name: "Interstellar",
+  poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+  rating: 8.6,
+  summary:
+  "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+  
+  },
+  {
+  name: "Baahubali",
+  poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+  rating: 8,
+  summary:
+  "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+  
+  },
+  {
+  name: "Ratatouille",
+  poster:
+  "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+  rating: 8,
+  summary:
+  "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+  
+  }
+  ];
   return (
 <div className="App">
-{students.map((student) => (
+{/* {students.map((student) => (
 <Message name={student.name} pic={student.pic}  />
-  ))}
+  ))} */}
 
 {/* {names.map((nm)=> 
   (<Welcome name={nm}/>))} */}
 
+<div className="movie-list">
+  {movieList.map((mv, index) => (<Movie movie={index} movie={mv} />
+  ))}
+  </div>
     </div>
   );
 }
-export default  App;
 
+
+function Movie({movie}){
+const styles = {
+  color: movie.rating > 8 ? "green" : "red",
+};
+const [show, setShow] = useState(true);
+
+const paraStyles = {
+  disply: show ? "block" : "none",
+};
+
+  return (
+     <div className="movie-container">
+    <img src={movie.poster} alt={movie.name} className="movie-poster" />
+    <div className="movie-specs">
+<h2 className="movie-name">{movie.name}</h2>
+<p style={styles} className="movie-rating">⭐{movie.rating}</p>
+     </div>
+
+<button onClick={() => setShow(!show)}>Toggle summary</button>
+
+
+<p style={paraStyles} className="movie-summary">
+  {movie.summary}
+</p>
+
+<Counter />
+</div>
+);
+}
+
+
+export default  App;
+// const [state, setState] = useState(InitialValue);
+function Counter() {
+  // let like = 10;
+  const [like, setLike] = useState(0);
+  const [dislike, setDisLike] = useState(0);
+
+  return (
+  <div className="counter-container">
+    <button className="like-button" onClick={() => setLike(like + 1)}>👍 {like}</button>
+    <button className="dislike-button" onClick={() => setDisLike(like + 1)}>👎 {dislike}</button>
+   
+  </div>
+  );
+}
+ 
 
 function Welcome(props){
   console.log(props);
@@ -74,6 +164,7 @@ function Message({pic,name}){
     <div>
     <img className="profile-pic" src={pic} alt={name}/>
         <h1>Hello, {name} 💜⟭⟬💜</h1>
+        <Counter />
     </div>
   );
 } 
